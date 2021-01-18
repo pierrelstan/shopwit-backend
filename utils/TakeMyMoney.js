70c5bb2d2241d1e956373a1427474916c313ccf2
Stanley <stanleypl75@gmail.com>
2021-01-18 18:11:46 -0400

    Create utils

---
+++ utils/TakeMyMoney.js
@@ -0,0 +1,11 @@
+module.exports = function formatMoney(amount) {
+  const options = {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 2,
+  };
+  if (amount % 100 === 0) options.minimumFractionDigits = 0;
+  const formatter = new Intl.NumberFormat('en-US', options);
+  return amount;
+  // formatter.format(amount / 100);
+};
