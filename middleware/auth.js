40610d58efbae3f2e4dfcf24f8d4b83d8a94495b
Stanley <stanleypl75@gmail.com>
2021-01-18 18:07:11 -0400

    :recycle: Remove comments

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,18 +1,14 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
-  // console.log(req);
   const token = req.header('x-auth-token');
-  // console.log(req.headers.authorization);
   if (!token) {
-    // console.log(token);
     return res.status(401).json({
       msg: 'No token, authorization denied',
     });
   }
   try {
     // verify token
-
     const decodeToken = jwt.verify(token, process.env.RANDOM_TOKEN_SECRET);
     const user = decodeToken.user;
     req.user = user;
