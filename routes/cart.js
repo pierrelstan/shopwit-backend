36f52975ec348f5acf61914821fa4d707c322a10
Stanley <stanleypl75@gmail.com>
2021-02-12 19:50:06 -0400

    Rename to carts

--- routes/cart.js
+++
@@ -1,6 +0,0 @@
-// const express = require("express");
-// const router = express.Router();
-
-// const CartCtrl = require("../controllers/cart.js");
-
-// router.post("/add-to-cart/:id", CartCtrl.addToCart);
