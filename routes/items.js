d7301ff402b45424e62d68d5a857d4673be11687
Stanley <stanleypl75@gmail.com>
2021-02-12 19:49:19 -0400

    Remove routes carts

---
+++ routes/items.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = express.Router();
+
+const ItemCtrl = require('../controllers/item.js');
+// const OrderCtrl = require("../controllers/order.js");
+
+const auth = require('../middleware/auth');
+
+// retrieving all
+router.use('/all', ItemCtrl.getAllItem);
+// retrieving heigth last item
+router.get('/lastproducts', ItemCtrl.getHeigthlastItems);
+// create a new item
+router.post('/new', auth, ItemCtrl.createItem);
+// retrieving one item
+router.get('/:id', ItemCtrl.getOneItem);
+// edit item
+router.put('/:id', auth, ItemCtrl.modifyItem);
+// delete an item
+router.delete('/:id', auth, ItemCtrl.deleteItem);
+router.get('/user/items/:id', auth, ItemCtrl.getAllItemsByUser);
+// search items
+router.get('/s/search?', ItemCtrl.searchItems);
+
+// pagination items
+router.get('/page/:page', ItemCtrl.getPaginationItems);
+
+module.exports = router;
