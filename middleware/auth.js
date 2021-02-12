6a8a27f7b10e0a50fd850076e33a26cc21c2174c
Stanley <stanleypl75@gmail.com>
2021-02-12 19:55:44 -0400

    Remove decodeToken

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,17 +1,13 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
-  const token = req.header('x-auth-token');
-  if (!token) {
-    return res.status(401).json({
-      msg: 'No token, authorization denied',
-    });
-  }
   try {
-    // verify token
-    const decodeToken = jwt.verify(token, process.env.RANDOM_TOKEN_SECRET);
-    const user = decodeToken.user;
-    req.user = user;
+    const token = req.header('x-auth-token');
+    if (!token) {
+      return res.status(401).json({
+        msg: 'No token, authorization denied',
+      });
+    }
     next();
   } catch (error) {
     res.status(401).json({ msg: error });
