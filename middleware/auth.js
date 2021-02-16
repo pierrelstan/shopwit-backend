6e442828f737b6e8af424e4fad8a6c48aeb2136e
Stanley <stanleypl75@gmail.com>
2021-02-16 10:40:43 -0400

    Add user id in the header request

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,13 +1,17 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
+  const token = req.header('x-auth-token');
+  if (!token) {
+    return res.status(401).json({
+      msg: 'No token, authorization denied',
+    });
+  }
   try {
-    const token = req.header('x-auth-token');
-    if (!token) {
-      return res.status(401).json({
-        msg: 'No token, authorization denied',
-      });
-    }
+    // verify token
+    const decodeToken = jwt.verify(token, process.env.RANDOM_TOKEN_SECRET);
+    const user = decodeToken.user;
+    req.user = user;
     next();
   } catch (error) {
     res.status(401).json({ msg: error });
