fd4eb70633cd54868628855300460f3016dcfb93
Stanley <stanleypl75@gmail.com>
2021-02-16 16:09:48 -0400

    Updates headers

--- app.js
+++ app.js
@@ -41,16 +41,29 @@ app.use(
   }),
 );
 app.use(function (req, res, next) {
-  res.header(
+  // Website you wish to allow to connect
+  res.setHeader(
     'Access-Control-Allow-Origin',
-    'http://pierrelstan.github.io/shopwitapp',
+    'https://pierrelstan.github.io/shopwitapp',
   );
-  res.header('Access-Control-Allow-Credentials', true);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header(
+
+  // Request methods you wish to allow
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, OPTIONS, PUT, PATCH, DELETE',
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
     'Access-Control-Allow-Headers',
-    'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json',
+    'X-Requested-With,content-type',
   );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
   next();
 });
 app.use('/api/item', itemRoutes);
