02997d1376c34dc433261a1a95623f1c47e4a8a6
Stanley <stanleypl75@gmail.com>
2021-02-17 15:13:56 -0400

    Update header

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.use(
 // Add headers
 app.use(function (req, res, next) {
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', '*');
 
   // Request methods you wish to allow
   res.setHeader(
