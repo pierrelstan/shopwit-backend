4bdfd9cec3b59c506a30d99569aad00eec63a5de
Stanley <stanleypl75@gmail.com>
2021-02-17 19:32:19 -0400

    Update header

--- app.js
+++ app.js
@@ -32,6 +32,7 @@ app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Headers', '*');
 
   if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Origin', '*');
     res.header(
       'Access-Control-Allow-Methods',
       'GET, POST, PUT, PATCH, DELETE,PATCH,OPTIONS',
