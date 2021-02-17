b1416e96c4ee1b4405f589126182b22c9dd52f85
Stanley <stanleypl75@gmail.com>
2021-02-16 23:05:29 -0400

    Update cors

--- app.js
+++ app.js
@@ -13,14 +13,27 @@ const ratingRoutes = require('./routes/rating');
 const favoritesRoutes = require('./routes/favorites');
 const cartsRoutes = require('./routes/carts');
 
-var corsOptions = {
-  origin: 'https://pierrelstan.github.io',
-  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-};
 const app = express();
 app.use(helmet());
 app.use(compression()); //Compress all routes
-app.use(cors(corsOptions));
+app.use(cors());
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  // Request methods you wish to allow
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, OPTIONS, PUT, PATCH, DELETE',
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,X-Access-Token,XKey,Authorization',
+  );
+
+  next();
+});
 mongoose
   .connect(process.env.MONGODB_API_KEY, {
     useNewUrlParser: true,
