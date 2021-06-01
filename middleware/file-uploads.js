156a0c120d98b8e6b9ba2ab26eacaad39d9a4301
pierrelstan <stanleypl75@gmail.com>
2021-06-01 07:25:39 -0400

    Remove destination & filename

--- middleware/file-uploads.js
+++ middleware/file-uploads.js
@@ -4,26 +4,18 @@ const nodeid = require('node-id');
 let uniqueId = nodeid();
 
 const MINE_TYPE_MAP = {
-  'image/png': 'png',
-  'image/jpeg': 'jpeg',
-  'image/jpg': 'jpg',
+    'image/png': 'png',
+    'image/jpeg': 'jpeg',
+    'image/jpg': 'jpg',
 };
 const fileUpload = multer({
-  limits: 500000,
-  storage: multer.diskStorage({
-    destination: (req, file, cb) => {
-      cb(null, 'uploads/images');
-    },
-    filename: (req, file, cb) => {
-      const ext = MINE_TYPE_MAP[file.mimetype];
-      cb(null, uniqueId + '.' + ext);
-    },
+    limits: 500000,
+    storage: multer.diskStorage({}),
     fileFilter: (req, file, cb) => {
-      const isValid = !!MINE_TYPE_MAP[file.mimetype];
-      let error = isValid ? null : new Error('Invalid mine type');
-      cb(error, isValid);
+        const isValid = !!MINE_TYPE_MAP[file.mimetype];
+        let error = isValid ? null : new Error('Invalid mine type');
+        cb(error, isValid);
     },
-  }),
 });
 
 module.exports = fileUpload;
