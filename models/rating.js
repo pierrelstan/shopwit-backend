3c4eb6296deeda435cb9cd14d244bd699c57e16e
pierrelstan <stanleypl75@gmail.com>
2021-06-03 22:19:10 -0400

    Configure rating model

--- models/rating.js
+++ models/rating.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
 
 const ratingSchema = mongoose.Schema({
-  userId: { type: mongoose.Schema.ObjectId, ref: 'User', require: true },
-  rating: { type: Number, required: true },
-  avgRating: { type: Number },
+    userId: { type: mongoose.Schema.ObjectId, ref: 'User', require: true },
+    item: { type: mongoose.Schema.ObjectId, ref: 'Item', require: true },
+    rating: { type: Number },
+    avgRating: { type: Number },
 });
 
 module.exports = mongoose.model('Rating', ratingSchema);
