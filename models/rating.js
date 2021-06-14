98b6a7f02c91e6bbb7a64ca88725b1b6d027b4e8
pierrelstan <stanleypl75@gmail.com>
2021-06-14 11:27:11 -0400

    Add count

--- models/rating.js
+++ models/rating.js
@@ -3,8 +3,9 @@ const mongoose = require('mongoose');
 const ratingSchema = mongoose.Schema({
     userId: { type: mongoose.Schema.ObjectId, ref: 'User', require: true },
     item: { type: mongoose.Schema.ObjectId, ref: 'Item', require: true },
-    rating: { type: Number },
-    avgRating: { type: Number },
+    rating: { type: Number, default: 0 },
+    avgRating: { type: Number, default: 0 },
+    count: { type: Number, default: 0 },
 });
 
 module.exports = mongoose.model('Rating', ratingSchema);
