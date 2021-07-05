1ad7a0f8f5fc5988b82762656b4fabf6f10b1027
Stanley <stanleypl75@gmail.com>
2021-07-04 22:06:38 -0400

    Updates

--- controllers/item.js
+++ controllers/item.js
@@ -8,212 +8,267 @@ const cloudinary = require('../middleware/cloudinary');
 const { ObjectId } = require('bson');
 
 exports.createProduct = async (req, res, next) => {
-    const { genre, title, quantity, description } = req.body;
-    const result = await cloudinary.uploader.upload(req.file.path, {
-        upload_preset: 'ecommerce',
-    });
-    let money = TakeMyMoney(req.body.price);
-    let item = new Item({
-        genre: genre,
-        title: title,
-        description: description,
-        imageUrl: result.secure_url,
-        cloudinary_id: result.public_id,
-        price: money,
-        quantityProducts: quantity,
-        userId: req.user.userId,
-    });
-    item.save((error, data) => {
-        if (error) {
-            res.status(400).json({
-                error: error,
-            });
-        } else {
-            res.status(201).json({
-                message: 'Item create successfully!',
-            });
-        }
-    });
+  const { genre, title, quantity, description } = req.body;
+  const result = await cloudinary.uploader.upload(req.file.path, {
+    upload_preset: 'ecommerce',
+  });
+  let money = TakeMyMoney(req.body.price);
+  let item = new Item({
+    genre: genre,
+    title: title,
+    description: description,
+    imageUrl: result.secure_url,
+    cloudinary_id: result.public_id,
+    price: money,
+    quantityProducts: quantity,
+    userId: req.user.userId,
+  });
+  item.save((error, data) => {
+    if (error) {
+      res.status(400).json({
+        error: error,
+      });
+    } else {
+      res.status(201).json({
+        message: 'Item create successfully!',
+      });
+    }
+  });
 };
 
 exports.getOneItem = (req, res, next) => {
-    const pipeline = [
-        {
+  const pipeline = [
+    {
+      $match: {
+        _id: ObjectId(req.params.id),
+      },
+    },
+    {
+      $lookup: {
+        from: 'ratings',
+        let: {
+          id: '$_id',
+        },
+        pipeline: [
+          {
             $match: {
-                _id: ObjectId(req.params.id),
+              $expr: {
+                $eq: ['$item', '$$id'],
+              },
             },
-        },
-        {
-            $lookup: {
-                from: 'ratings',
-                let: {
-                    id: '$_id',
-                },
-                pipeline: [
-                    {
-                        $match: {
-                            $expr: {
-                                $eq: ['$item', '$$id'],
-                            },
-                        },
-                    },
-                    {
-                        $project: {
-                            rating: 1,
-                        },
-                    },
-                ],
-                as: 'ratings',
+          },
+          {
+            $project: {
+              rating: 1,
             },
-        },
-    ];
+          },
+        ],
+        as: 'ratings',
+      },
+    },
+  ];
 
-    Item.aggregate(pipeline)
-        .then((item) => {
-            res.status(200).json(item[0]);
-        })
-        .catch((error) => {
-            res.status(404).json({
-                error: error,
-            });
-        });
+  Item.aggregate(pipeline)
+    .then((item) => {
+      res.status(200).json(item[0]);
+    })
+    .catch((error) => {
+      res.status(404).json({
+        error: error,
+      });
+    });
 };
 
 exports.getAllItem = (req, res, next) => {
-    Item.find({})
-        .sort('-created')
-        .lean()
-        .then((items) => {
-            res.status(200).json(items);
-        })
-        .catch((error) => {
-            res.status(400).json({
-                error: error,
-            });
-        });
+  Item.find({})
+    .limit(8)
+    .sort('-created')
+    .lean()
+    .then((items) => {
+      res.status(200).json(items);
+    })
+    .catch((error) => {
+      res.status(400).json({
+        error: error,
+      });
+    });
 };
 
 exports.getHeigthlastItems = (req, res, next) => {
-    Item.find({})
-        .limit(8)
-        .sort('-created')
-        .lean()
-        .then((items) => {
-            res.status(201).json(items);
-        })
-        .catch((error) => {
-            res.status(400).json({
-                error: error,
-            });
-        });
+  let pipeline = [
+    {
+      $lookup: {
+        from: 'ratings',
+        let: {
+          id: '$_id',
+        },
+        pipeline: [
+          {
+            $match: {
+              $expr: {
+                $eq: ['$item', '$$id'],
+              },
+            },
+          },
+          {
+            $project: {
+              rating: 1,
+            },
+          },
+        ],
+        as: 'ratings',
+      },
+    },
+    {
+      $unwind: {
+        path: '$ratings',
+      },
+    },
+    {
+      $group: {
+        _id: '$_id',
+        count: {
+          $sum: 1,
+        },
+        totalRatings: {
+          $sum: '$ratings.rating',
+        },
+        item: {
+          $push: {
+            _id: '$_id',
+            title: '$title',
+            description: '$description',
+            imageUrl: '$imageUrl',
+            cloudinary_id: '$cloudinary_id',
+            price: '$price',
+            quantityProducts: '$quantityProducts',
+            userId: '$userId',
+          },
+        },
+      },
+    },
+    {
+      $sort: {
+        totalratings: -1,
+      },
+    },
+    {
+      $limit: 5,
+    },
+  ];
+
+  Item.aggregate(pipeline)
+    .then((items) => {
+      res.status(201).json(items);
+    })
+    .catch((error) => {
+      res.status(400).json({
+        error: error,
+      });
+    });
 };
 
 exports.getPaginationItems = (req, res, next) => {
-    let ITEM_PER_PAGE = 8;
-    let page = parseInt(req.params.page);
-    let skip = (page - 1) * ITEM_PER_PAGE;
-    Item.find()
-        .skip(skip)
-        .limit(ITEM_PER_PAGE)
-        .sort('-created')
-        .then((items) => {
-            return res.status(200).json(items);
-        })
-        .catch((error) => {
-            res.status(400).json({
-                error: error,
-            });
-        });
+  let ITEM_PER_PAGE = 8;
+  let page = parseInt(req.params.page);
+  let skip = (page - 1) * ITEM_PER_PAGE;
+  Item.find()
+    .skip(skip)
+    .limit(ITEM_PER_PAGE)
+    .sort('-created')
+    .then((items) => {
+      return res.status(200).json(items);
+    })
+    .catch((error) => {
+      res.status(400).json({
+        error: error,
+      });
+    });
 };
 
 exports.modifyItem = async (req, res, next) => {
-    let money = TakeMyMoney(req.body.price);
-    let id = req.params.id;
-    const result = await cloudinary.uploader.upload(req.file.path, {
-        upload_preset: 'ecommerce',
+  let money = TakeMyMoney(req.body.price);
+  let id = req.params.id;
+  const result = await cloudinary.uploader.upload(req.file.path, {
+    upload_preset: 'ecommerce',
+  });
+  const item = new Item({
+    _id: id,
+    title: req.body.title,
+    description: req.body.description,
+    price: money,
+    imageUrl: result.secure_url,
+    cloudinary_id: result.public_id,
+    quantityProducts: req.body.quantity,
+    userId: req.user.userId,
+  });
+  Item.updateOne(
+    {
+      _id: req.params.id,
+    },
+    item,
+  )
+    .lean()
+    .then(() => {
+      res.status(201).json({
+        message: 'Item Updated successfully!',
+      });
+    })
+    .catch((error) => {
+      res.status(400).json({
+        error: error,
+      });
     });
-    const item = new Item({
-        _id: id,
-        title: req.body.title,
-        description: req.body.description,
-        price: money,
-        imageUrl: result.secure_url,
-        cloudinary_id: result.public_id,
-        quantityProducts: req.body.quantity,
-        userId: req.user.userId,
-    });
-    Item.updateOne(
-        {
-            _id: req.params.id,
-        },
-        item
-    )
-        .lean()
-        .then(() => {
-            res.status(201).json({
-                message: 'Item Updated successfully!',
-            });
-        })
-        .catch((error) => {
-            res.status(400).json({
-                error: error,
-            });
-        });
 };
 
 exports.deleteItem = async (req, res, next) => {
-    let itemById = await Item.findById({ _id: req.params.id }).lean();
-    await cloudinary.uploader.destroy(itemById.cloudinary_id);
+  console.log(req.params.id);
+  let itemById = await Item.findById({ _id: req.params.id }).lean();
+  await cloudinary.uploader.destroy(itemById.cloudinary_id);
 
-    let { userId } = itemById;
-    if (userId.equals(req.user.userId)) {
-        try {
-            await Item.findOneAndDelete({
-                _id: req.params.id,
-            });
-        } catch (error) {
-            res.status(400).json({
-                error: error,
-            });
-        }
-    } else {
-        res.status(400).json({
-            error: 'Item not belongs to you , access denied!',
-        });
-    }
+  try {
+    await Item.findOneAndDelete({
+      _id: req.params.id,
+      userId: req.user.userId,
+    }).lean();
+  } catch (error) {
+    res.status(400).json({
+      error: error,
+    });
+  }
 };
 
 exports.getAllItemsByUser = (req, res, next) => {
-    Item.find({ userId: req.params.id })
-        .sort('-created')
-        .lean()
-        .then((item) => {
-            res.status(200).json(item);
-        })
-        .catch((err) => {
-            res.status(400).json({
-                err: err,
-            });
-        });
+  Item.find({ userId: req.params.id })
+    .sort('-created')
+    .lean()
+    .then((item) => {
+      res.status(200).json(item);
+    })
+    .catch((err) => {
+      res.status(400).json({
+        err: err,
+      });
+    });
 };
 
 function escapeRegex(text) {
-    return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&');
+  return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&');
 }
 
 exports.searchItems = (req, res, next) => {
-    res.header('Content-Type', 'application/json');
-    res.header('Access-Control-Allow-Origin', '*');
-    const regex = new RegExp(escapeRegex(req.query.title), 'gi');
-    Item.find(
-        {
-            title: regex,
-        },
-        function (err, items) {
-            if (err) res.status(401).json(err);
-            else {
-                res.status(201).json(items);
-            }
-        }
-    ).lean();
+  res.header('Content-Type', 'application/json');
+  res.header('Access-Control-Allow-Origin', '*');
+  const regex = new RegExp(escapeRegex(req.query.title), 'gi');
+  Item.find(
+    {
+      title: regex,
+    },
+    function (err, items) {
+      if (err) res.status(401).json(err);
+      else {
+        res.status(201).json(items);
+      }
+    },
+  ).lean();
 };
