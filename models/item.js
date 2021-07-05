f8f4db3b9d4c949b127b21cec96ceb73e97e8038
Stanley <stanleypl75@gmail.com>
2021-07-04 22:04:53 -0400

    Updates

--- models/item.js
+++ models/item.js
@@ -2,22 +2,23 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const itemSchema = Schema({
-    // genre: { type: String, require: true },
-    title: { type: String },
-    description: { type: String },
-    imageUrl: { type: String },
-    price: { type: Number },
-    quantityProducts: { type: Number },
-    userId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        require: true,
-    },
-    ratings: { type: mongoose.Schema.ObjectId, ref: 'Rating' },
-    rating: { type: Number, min: 1, max: 5 },
-    cloudinary_id: { type: String },
-    created: { type: Date, default: Date.now },
-    updated_at: { type: Date, default: Date.now },
+  // genre: { type: String, require: true },
+  title: { type: String },
+  description: { type: String },
+  imageUrl: { type: String },
+  price: { type: Number },
+  quantityProducts: { type: Number },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    require: true,
+  },
+  ratings: { type: mongoose.Schema.ObjectId, ref: 'Rating' },
+  rating: { type: Number, min: 1, max: 5 },
+  cloudinary_id: { type: String },
+  ratings: { type: mongoose.Schema.ObjectId, ref: 'Rating' },
+  created: { type: Date, default: Date.now },
+  updated_at: { type: Date, default: Date.now },
 });
 
 module.exports = mongoose.model('Item', itemSchema);
