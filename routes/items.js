3afea47e6a5a95ada2eafdfd8baa0821883fc53d
Stanley <stanleypl75@gmail.com>
2021-07-14 20:32:36 -0400

    Add routes page for specific gender items

--- routes/items.js
+++ routes/items.js
@@ -7,6 +7,8 @@ const fileUpload = require('../middleware/file-uploads');
 router.get('/', ItemCtrl.getAllItem);
 // retrieving heigth last item
 router.get('/lastproducts', ItemCtrl.getHeigthlastItems);
+// count  differents types items
+router.get('/counts', ItemCtrl.queryCountTypes);
 // create a new item
 router.post('/new', fileUpload.single('image'), auth, ItemCtrl.createProduct);
 // retrieving one item
@@ -21,5 +23,9 @@ router.get('/s/search?', ItemCtrl.searchItems);
 
 // pagination items
 router.get('/page/:page', ItemCtrl.getPaginationItems);
+// pagination for male female children items
+router.get('/page/m/:page', ItemCtrl.queryMaleItems);
+router.get('/page/f/:page', ItemCtrl.queryFemaleItems);
+router.get('/page/c/:page', ItemCtrl.queryChildrenItems);
 
 module.exports = router;
