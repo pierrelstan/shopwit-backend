d588f7a3b9a269d246b99068e148c736b27c963d
Stanley <stanleypl75@gmail.com>
2021-07-18 16:07:44 -0400

    Update carts with array ObjectId

--- models/order.js
+++ models/order.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const orderSchema = mongoose.Schema({
-  userId: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
-  carts: {},
-  subTotal: {type: String, require: true},
-  created: {type: Date, default: Date.now},
-  updated_at: {type: Date, default: Date.now},
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  carts: { type: [mongoose.Schema.Types.ObjectId], ref: 'Cart' },
+  subTotal: { type: String, require: true },
+  created: { type: Date, default: Date.now },
+  updated_at: { type: Date, default: Date.now },
 });
 
 module.exports = mongoose.model('Order', orderSchema);
