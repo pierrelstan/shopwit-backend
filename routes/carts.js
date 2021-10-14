e1009a28a0d533c660dbf5607eb9e86b80bb9518
Stanley <stanleypl75@gmail.com>
2021-10-14 10:09:17 -0400

    Add route remove carts

--- routes/carts.js
+++ routes/carts.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-
+const OrderCtrl = require('../controllers/order.js');
 const CartCtrl = require('../controllers/cart.js');
 const auth = require('../middleware/auth');
 
@@ -8,5 +8,6 @@ router.post('/:id', auth, CartCtrl.addToCart);
 router.get('/:id', auth, CartCtrl.findCartByUserId);
 router.delete('/remove/:id', auth, CartCtrl.removeCartById);
 router.put('/:id', auth, CartCtrl.updateCart);
+router.post('/remove/carts', auth, CartCtrl.removeCartsIdsAfterTheOrdering);
 
 module.exports = router;
