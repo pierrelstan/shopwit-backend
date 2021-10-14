36b67cd81596941b778813265c6a40767d3dd298
Stanley <stanleypl75@gmail.com>
2021-10-14 10:10:18 -0400

    Add rates

--- routes/rating.js
+++ routes/rating.js
@@ -5,7 +5,7 @@ const RatingCtrl = require('../controllers/rating.js');
 const auth = require('../middleware/auth');
 
 router.get('/', RatingCtrl.queryAllRatingsAverage);
-router.get('/:id', auth, RatingCtrl.findRatingsByUserId);
+router.get('/rates/:id', auth, RatingCtrl.findRatingsByUserId);
 router.post('/:id', auth, RatingCtrl.createRatingItem);
 
 module.exports = router;
